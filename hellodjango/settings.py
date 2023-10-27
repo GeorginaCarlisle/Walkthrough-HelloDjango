0d528bbe11499954e8fc507366c3c67e4ac324ed
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-10-27 10:29:58 +0100

    Add secret keys

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-vn3-jrz0+px1j)nbzo81(bq@ubyc2v-gy*+_8=69zs2v=*!*6g')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
