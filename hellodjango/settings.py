146153a299e641ab95827db6fe8d4c231fbc4af3
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-10-27 09:49:29 +0100

    Set up environmental variables and automatic deployment

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vn3-jrz0+px1j)nbzo81(bq@ubyc2v-gy*+_8=69zs2v=*!*6g'
+SECRET_KEY = os.envrion.get('SECRET_KEY', 'django-insecure-vn3-jrz0+px1j)nbzo81(bq@ubyc2v-gy*+_8=69zs2v=*!*6g')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['walkthrough-hello-django-a3c05bab85a6.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
